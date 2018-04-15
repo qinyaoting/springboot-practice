af600b5fb4489ebfcdabb5fa09e18843045b3222
qyt <qinyaoting@gmail.com>
2018-04-15 20:58:01 +0800

    springboot

---
+++ ch6-read-properties/src/main/java/com/xyz/App.java
@@ -0,0 +1,40 @@
+package com.xyz;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chin
+ * Date: 4/15/18
+ * Time: 8:36 PM
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+@RestController
+@SpringBootApplication
+public class App {
+
+    @Value("${book.author}")
+    private String bookAuthor;
+
+    @Value("${book.name}")
+    private String bookName;
+
+    @RequestMapping("/")
+    String index() {
+        return "book name is :" + bookName + " author:" + bookAuthor +"\n"+
+                authorSettings.getName() + "--" + authorSettings.getAge();
+    }
+
+    @Autowired
+    private AuthorSettings authorSettings;
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ ch6-read-properties/src/main/java/com/xyz/AuthorSettings.java
@@ -0,0 +1,36 @@
+package com.xyz;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chin
+ * Date: 4/15/18
+ * Time: 8:49 PM
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+@Component
+@ConfigurationProperties(prefix = "author")
+public class AuthorSettings {
+
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
