b55f98b8fdc14f5b8d8d26d30f05c9e701b2f522
qyt <qinyaoting@gmail.com>
2020-01-08 11:40:59 +0800

    Runing fine

--- chf4-jpa-h2/src/main/java/com/xyz/H2App.java
+++ chf4-jpa-h2/src/main/java/com/xyz/H2App.java
@@ -1,11 +1,13 @@
-package com.xyz.app;
+package com.xyz;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class H2App {
 
     public static void main(String[] args) {
@@ -16,5 +18,8 @@ public class H2App {
     // h2-console
 
     //http://localhost:8089/h2-console/login.do?jsessionid=edd7da617270f696faf393b563fc6c4d
+
+
+    // http://localhost:8089/hello
 
 }
--- chf4-jpa-h2/src/main/java/com/xyz/bean/Location.java
+++ chf4-jpa-h2/src/main/java/com/xyz/bean/Location.java
@@ -3,18 +3,19 @@ package com.xyz.bean;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Location {
-    @Id
+
+    @Id  //不是 org.springframework.data.annotation.Id; 这个Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String type;
